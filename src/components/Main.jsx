e42a823892e0e3b87ef96aef1f228d877476bef2
shraddhak22 <spkharche22@gmail.com>
2020-02-01 17:51:43 -0500

    Added images

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,29 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import final_image from "../assets/final_image.png";
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
   render() {
     return (
       <main>
-        <h1>Chat with us today to get direct support
-        <br />
-          and personal guide for postpartum
-        </h1>
+        <div className="Container">
+          <div className="left-panel">
+            <h1>
+              Chat with us today to get direct support
+              <br />
+              and personal guide for postpartum
+            </h1>
 
-        <button type='button' className='btn-how-it-works'>
-          <h2>
-            How it works
-          </h2>
-        </button>
+            <button type="button" className="btn-how-it-works">
+              <h2>How it works</h2>
+            </button>
+          </div>
+          <div className="right-panel">
+            <img src={final_image} alt=""></img>
+          </div>
+        </div>
       </main>
     );
   }
