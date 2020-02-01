6aa024ec13ad9d488f71b420f1fb4a9a8120c713
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2020-02-01 18:28:04 -0500

    got shraddhas changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import final_image from "../assets/final_image.png";
 
 class Main extends Component {
   constructor(props) {
@@ -8,16 +9,21 @@ class Main extends Component {
   render() {
     return (
       <main>
-        <h1>Chat with us today to get direct support
-        <br />
-          and personal guide for postpartum
-        </h1>
-
-        <button type='button' className='btn-how-it-works'>
-          <h2>
-            How it works
-          </h2>
-        </button>
+        <div className="Container">
+          <div className="left-panel">
+            <h1>
+              Chat with us today to get direct support
+              <br />
+              and personal guide for postpartum
+            </h1>
+            <button type="button" className="btn-how-it-works">
+              <h2>How it works</h2>
+            </button>
+          </div>
+          <div className="right-panel">
+            <img src={final_image} alt=""></img>
+          </div>
+        </div>
       </main>
     );
   }
