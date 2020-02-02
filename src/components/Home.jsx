d729e021525c6b8f8f614d9c20d50c2d656cdbac
shraddhak22 <spkharche22@gmail.com>
2020-02-01 21:15:54 -0500

    Chatbox changes, added chat image icon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import Header from './Header';
-import Main from './Main';
-import Funnels from './Funnels';
-import Services from './Services';
+import React, { Component } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Funnels from "./Funnels";
+import Services from "./Services";
+import Chatbox_try from "./Chatbox_try";
 
 export default class Home extends Component {
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
 
   render() {
@@ -17,8 +18,8 @@ export default class Home extends Component {
         <Main />
         <Funnels />
         <Services />
+        <Chatbox_try />
       </div>
     );
   }
 }
-
